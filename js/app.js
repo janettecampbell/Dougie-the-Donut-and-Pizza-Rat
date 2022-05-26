98edccefd360b7ecaf1ed865dd2a645576e9f104
Janette Campbell <campbjp@hotmail.com>
2022-05-26 15:35:37 -0400

    added links

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
-fetch("https://data.cityofnewyork.us/resource/erm2-nwe9.json")
+let limit = 10;
+let borough = "Brooklyn";
+
+const bronx = document.querySelector("#bronx");
+const brooklyn = document.querySelector("#brooklyn");
+const manhattan = document.querySelector("#manhattan");
+const queens = document.querySelector("#queens");
+const statenIsland = document.querySelector("#staten island");
+
+bronx.addEventListener("click", () => {
+  "https://data.cityofnewyork.us/resource/erm2-nwe9.json?&borough=" +
+    bronx +
+    "&$limit=" +
+    limit;
+});
+
+fetch(
+  "https://data.cityofnewyork.us/resource/erm2-nwe9.json?&borough=" +
+    borough +
+    "&$limit=" +
+    limit
+)
   .then((response) => response.json())
   .then((json) => console.log(json))
   .catch((error) => console.log(error));
